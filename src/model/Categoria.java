0b5f8c464fc11286a30283bff442a9fbc7fa19f1
jpzyn <jpstere25@gmail.com>
2025-11-12 12:45:57 -0300

    refactor: adiciona getters e setters na classe Categoria

--- src/model/Categoria.java
+++ src/model/Categoria.java
@@ -12,8 +12,48 @@ public class Categoria {
     public enum Embalagem {
         LATA,
         VIDRO,
-        PLAASTICO,
+        PLASTICO
     }
 
-    // Construtor, getters e setters
+    public Categoria() {
+    }
+
+    public Categoria(String nome, Tamanho tamanho, Embalagem embalagem) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Embalagem getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(Embalagem embalagem) {
+        this.embalagem = embalagem;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "nome='" + nome + '\'' +
+                ", tamanho=" + tamanho +
+                ", embalagem=" + embalagem +
+                '}';
+    }
 }
